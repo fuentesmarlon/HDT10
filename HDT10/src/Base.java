ce8c14cf06e1b4c2f2fed38d19f958832dc816b0
fuentesmarlon <fue15240@uvg.edu.gt>
2016-11-07 16:02:22 -0600

    v 2.0
    v 2.0

--- HDT10/src/Base.java
+++ HDT10/src/Base.java
@@ -1,11 +1,18 @@
 
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.SingleGraph;
 
 
 
@@ -20,8 +27,17 @@ import java.util.logging.Logger;
  * @author Marlon
  */
 public class Base {
+    private String archivo="C:\\Users\\Marlon\\Documents\\universidad\\datos\\HDT10\\datos.csv";
+    Graph graph = new SingleGraph("Correos");
+    private String[][] matriz;
+
+    public Base() {
+       matriz = new String[15][15];
+    }
+   
+    
    /*1. hacer conexion*/
-    public void crearBase(){
+    public void conector(){
         try {
             Connection con = DriverManager.getConnection("jdbc:neo4j:bolt://localhost/?user=neo4j,password=1234,debug=true,noSsl,flatten=[-1,100,1000]");
             try (Statement stmt = con.createStatement()){
@@ -32,10 +48,36 @@ public class Base {
             }
             con.close();
         } catch(Exception ex){
-            System.out.println("ASS");
+            System.out.println("Error");
         }
     
     }
     
-
+    public void lector(){
+        BufferedReader br =null;
+       try{
+           String linea; 
+           br = new BufferedReader(new FileReader(archivo));
+           while((linea = br.readLine())!=null){
+               for(int i = 0;i<matriz.length;i++){
+                   for(int j = 0;j<matriz[i].length;j++){
+                       String [] datos = linea.split(";");
+                       matriz[i][j] = datos[i];
+                   }
+               }           
+           }
+       }
+       catch(IOException e){
+           System.out.println("Error");
+       }
+    }
+   public void agregarNodo(String nodo){
+       graph.addNode(nodo);
+   } 
+   public void agregarRelacion(String arista,String nodo1, String nodo2){
+       graph.addEdge(arista,nodo1,nodo2,true);
+   }
+   public void generarGrafo(){
+       graph.display();
+   }
 }
